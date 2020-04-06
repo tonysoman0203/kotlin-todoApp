068f2360770465b5535c4626d374d97cfa2b0d34
tonysoman0203 <somankit2000@gmail.com>
2020-04-06 17:25:48 +0800

    feat: add navigation button

--- app/src/main/java/com/example/kotlintodoapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlintodoapp/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.kotlintodoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.kotlintodoapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +14,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        if (supportActionBar != null) {
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.setDisplayShowHomeEnabled(true)
+        }
+        val navController = findNavController(this, R.id.main_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.main_fragment).navigateUp()
+                || super.onSupportNavigateUp()
     }
 }
